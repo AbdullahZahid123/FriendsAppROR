65c0ca8edc537565d41e85c225958ae944c9a9d4
Abdullah <ranaabdullahzahid123@gmail.com>
2023-03-09 20:30:43 +0500

    Products Page

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   get 'contact' => 'contact#contact'
   get 'about' => 'about#about'
+  get 'products' => 'products#products'
   root 'home#index'
 end
